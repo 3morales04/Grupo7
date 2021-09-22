7dc671291ddcc23e7773a4e611dc1572869cdd22
Luis-xmart <luis1248martinezvargas@gmail.com>
2021-09-22 18:58:55 -0500

    Función Validar Telefono

--- form.js
+++ form.js
@@ -47,9 +47,32 @@ function checkCorreo(valor) {
     }
 }
 
+//3. función para validar Telefono
+function checkTelefono(valor) {
+    const telefono = new RegExp("/^[0-9]{7}$/");
+    let isValid= false;
+    try {
+        if(isNaN(valor)){
+            throw "Valor vacio"
+        }else{
+            if (telefono.test(valor)){
+                isValid= true;
+            }else{
+                isValid= false;
+            }
+        }
+    } catch(error){
+        console.log(error);
+    }finally{
+        return isValid;
+    }
+}
+
+
 //Exportación de módulos
 
 
 
 module.exports = checkDir;
 module.exports = checkCorreo;
+module.exports = checkTelefono;

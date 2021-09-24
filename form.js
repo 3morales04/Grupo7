3b72b4b117eb046fbf72a67072c17a30afd1084f
nmbdev <developer.nmb@gmail.com>
2021-09-23 21:07:48 -0500

    Actualización form.js (test en checkNombre)

--- form.js
+++ form.js
@@ -1,14 +1,13 @@
 //1. función para validar Nombre
-/*
 function checkNombre(valor) {
-    const EXPRESION = new RegExp("^[A-Z]+$","i");// Letras y espacios, pueden llevar acentos.
+    const EXPRESION = /^[a-zA-ZÁ-ÿ\s]*$/; // Letras y espacios, pueden llevar acentos.
     let isValid = false;
 
     try{
         if(valor === ""){
             throw "valor vacío";
         }else {
-            if(String(valor).length < 4  && String(valor).length > 30 ){
+            if(valor.length < 4  && String(valor).length > 30 ){
                 throw "valor excede el tamaño permitido";
             } else{
                 if(EXPRESION.test(valor)){
@@ -22,33 +21,17 @@ function checkNombre(valor) {
         return isValid;
     }
 }
-*/
-
-//1. función para validar Nombre
-function checkNombre(valor) {
-    if (valor == "") {
-        alert("Escriba su Nombre");
-        valor.focus();
-        return false;
-    }
-    if (!/^[a-zA-Z]*$/g.test(valor)) {
-        alert("Caracteres inválidos, solo letras");
-        valor.focus();
-        return false;
-    }
-}
-
 
 //4. función para validar DIRECCIÓN
 function checkDir(valor) {
-    const EXPRESION = new RegExp("^[0-9a-zA-Z\#+\-?\s*]*$");
+    const EXPRESION = /^[0-9a-zA-Z\#\-\s]*$/;
     let isValid = false;
 
     try{
         if(valor === ""){
             throw "valor vacío";
         }else {
-            if(String(valor).length > 50){
+            if(valor.length > 50){
                 throw "valor excede el tamaño permitido";
             } else{
                 if(EXPRESION.test(valor)){
@@ -62,17 +45,18 @@ function checkDir(valor) {
         return isValid;
     }
 }
+//console.log(checkDir("Calle 1O # 2-3"));
 
 //5. función para validar CORREO
 function checkCorreo(valor) {
-    const EXPRESION = new RegExp("^[a-zA-Z]+\@[a-zA-Z]+\.[a-zA-Z]+$");
+    const EXPRESION = /^[a-zA-Z]+\@[a-zA-Z]+\.[a-zA-Z]+$/;
     let isValid = false;
 
     try{
         if(valor === ""){
             throw "valor vacío";
         }else {
-            if(String(valor).length > 50){
+            if(valor.length > 50){
                 throw "valor excede el tamaño permitido";
             } else{
                 if(EXPRESION.test(valor)){
@@ -102,13 +86,16 @@ function checkTelefono(valor) {
 }
 
 //Exportación de módulos
-module.export = checkDir;
-module.export = checkCorreo;
-module.export = checkTelefono;
-module.export = checkNombre;
+/*
+module.exports = checkDir;
+module.exports = checkCorreo;
+module.exports = checkTelefono;
+module.exports = checkNombre;
+*/
+
 //TEST
 //console.log(checkDir("Calle 1O # 2-3"));
 //console.log(checkDir("12th Simon st"));
 //console.log(checkCorreo("usuario@correo.com"));
 //console.log(checkTelefono(232343));
-//console.log(checkNombre("Nombre Apellido"));
+console.log(checkNombre("Nombre Apellido"));

0ba5e1010cd7b65fabcae2872151bea972dd0a6c
nmbdev <developer.nmb@gmail.com>
2021-10-13 14:39:04 -0500

    Creació función validarCAPTCH()

--- login.js
+++ login.js
@@ -6,11 +6,44 @@ function login(){
 function agregarRegistro(){
 }
 
+//3. Función validarCAPTCHA
 function validarCAPTCHA(valor){
+    let isValid = false;
+    const ANSWER = "ocaso";
+    const EXPRESION = /[^a-zA-Z0-9]/g; //!
 
+    try{
+        valor = valor.replace(EXPRESION,"");
+
+        switch (valor.toLowerCase()){
+            case "":
+                throw "CAPTCHA vacio";
+                break;
+            case ANSWER:
+                isValid = true;
+                break;
+            default:
+                throw "CAPTCHA incorrecto";
+        }
+    }catch(error){
+        console.log(error)
+    }finally{
+        //console.log(valor)
+        return isValid;
+    }
 }
 
-module.exports.login = login;
+//Exportación variables y funciones
 module.exports.registros = registros;
+module.exports.login = login;
 module.exports.validarCAPTCHA = validarCAPTCHA;
 module.exports.agregarRegistro = agregarRegistro;
+
+/*
+module.exports = {
+    registros,
+    login,
+    agregarRegistro,
+    validarCAPTCHA,
+}
+*/

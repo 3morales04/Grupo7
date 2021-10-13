23804cf7cd0f4dc60328bed0055615b040ae4743
nmbdev <developer.nmb@gmail.com>
2021-10-13 10:47:10 -0500

    Creación archivo login.js

---
+++ login.js
@@ -0,0 +1,16 @@
+let registros = [];
+
+function login(){
+}
+
+function agregarRegistro(){
+}
+
+function validarCAPTCHA(valor){
+
+}
+
+module.exports.login = login;
+module.exports.registros = registros;
+module.exports.validarCAPTCHA = validarCAPTCHA;
+module.exports.agregarRegistro = agregarRegistro;
